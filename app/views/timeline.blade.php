d38d353a11f96695421110530f9eba793f76d87f
Kai Schwierczek <kai@schwierczek.de>
2014-06-10 15:29:04 +0200

    make navbar more modular

--- app/views/timeline.blade.php
+++ app/views/timeline.blade.php
@@ -1,9 +1,5 @@
 @extends('layouts/main')
 
-@section('conf')
-	<?php $conf['currentPage'] = 'timeline'; ?>
-@stop
-
 @section('head')
 		<script src="//cdn.datatables.net/1.10.0/js/jquery.dataTables.js"></script>
 		<script src="//cdn.datatables.net/plug-ins/28e7751dbec/integration/bootstrap/3/dataTables.bootstrap.js"></script>
