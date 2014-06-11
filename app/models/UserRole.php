041d4bbe8dac6cf66188029c67a0a1fb705a1463
Kai Schwierczek <kai@schwierczek.de>
2014-06-11 16:51:26 +0200

    remove legacy relationship method

--- app/models/UserRole.php
+++ app/models/UserRole.php
@@ -1,46 +1,39 @@
 <?php
 
 class UserRole extends Eloquent {
-        
-
-        const SUPER_ADMIN = 1;
-        const LAB_LEADER = 2;
-        const GROUP_LEADER = 3;
+	const SUPER_ADMIN = 1;
+	const LAB_LEADER = 2;
+	const GROUP_LEADER = 3;
     
 	public function user()
 	{
 		return $this->belongsTo('User');
 	}
-
-	public function role()
-	{
-		return $this->morphTo();
+        
+	public static function getUserRole($roleId) {
+		return UserRole::getRoleFromUser(Auth::user(), $roleId);
+	}    
+	
+	public static function getRoleFromUser($user, $roleId) {
+		$roles = UserRole::where('user_id', '=', $user->id)->get();
+		foreach($roles as $role) {
+			if($role->role_id == $roleId) 
+				return $role;
+		}      
+		return null;            
 	}
-        
-        public static function getUserRole($roleId) {
-            return UserRole::getRoleFromUser(Auth::user(), $roleId);
-        }    
-        
-        public static function getRoleFromUser($user, $roleId) {
-            $roles = UserRole::where('user_id', '=', $user->id)->get();
-            foreach($roles as $role) {
-                if($role->role_id == $roleId) 
-                    return $role;
-            }      
-            return null;            
-        }
-        
-        public static function updateRole($user, $roleId, $isActive) {
-            $role = UserRole::getRoleFromUser($user, $roleId);
-            if($role != null) {
-                $role->active = $isActive;
-                return $role;
-            }
-            $newRole = new UserRole;
-            $newRole->user_id = $user->id;
-            $newRole->role_id = $roleId;
-            $newRole->active = $isActive;
-            $newRole->save();     
-            return $newRole;
-        }
+	
+	public static function updateRole($user, $roleId, $isActive) {
+		$role = UserRole::getRoleFromUser($user, $roleId);
+		if($role != null) {
+			$role->active = $isActive;
+			return $role;
+		}
+		$newRole = new UserRole;
+		$newRole->user_id = $user->id;
+		$newRole->role_id = $roleId;
+		$newRole->active = $isActive;
+		$newRole->save();     
+		return $newRole;
+	}
 }
