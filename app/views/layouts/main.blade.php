5ec30e1a3f803d52217256f0c8245bdad8ed062b
Kai Schwierczek <kai@schwierczek.de>
2014-06-11 16:45:04 +0200

    hide "My Review" as it has no functionality yet

--- app/views/layouts/main.blade.php
+++ app/views/layouts/main.blade.php
@@ -43,7 +43,7 @@
 					<ul class="pull-right nav nav-pills">
 						<li {{ (Route::current()->uri() == 'timeline') ? 'class="active"' : '' }}>{{ HTML::link('timeline', 'Timeline') }}</li>
 						<li {{ (Route::current()->uri() == 'paper') ? 'class="active"' : '' }}>{{ HTML::link('paper', 'My Paper') }}</li>
-						<li {{ (Route::current()->uri() == 'data') ? 'class="active"' : '' }}>{{ HTML::link('data', 'My Review') }}</li>
+						{{--<li {{ (Route::current()->uri() == 'data') ? 'class="active"' : '' }}>{{ HTML::link('data', 'My Review') }}</li>--}}
 						<li {{ (Route::current()->uri() == 'profile') ? 'class="active"' : '' }}>{{ HTML::link('profile', 'My Profile') }}</li>
 						<li {{ (Route::current()->uri() == 'conferences') ? 'class="active"' : '' }}>{{ HTML::link('conferences', 'Conferences') }}</li>
 						@if (Auth::user()->isAdmin())
