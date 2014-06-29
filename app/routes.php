4aad502c7d24d721fb19f916b54009bd0ae2e67f
Kai Schwierczek <kai@schwierczek.de>
2014-06-29 18:05:49 +0200

    remove standalone submission edit page
    submissions will be edited in the timeline or paper detail/edit view

--- app/routes.php
+++ app/routes.php
@@ -38,7 +38,6 @@ Route::group(array('before' => 'auth'), function()
 	Route::controller('papermanager', 'PaperManagerController');
 
 	Route::controller('review', 'ReviewController');
-	Route::controller('submissions', 'SubmissionController');
 
 	Route::get('overview', function() 
 	{
