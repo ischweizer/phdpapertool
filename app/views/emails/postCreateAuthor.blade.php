8efd7ac94c9e058b4d25c162367ea51210932695
Anton Rohr <anton.rohr@gmx.de>
2014-08-07 17:04:45 +0200

    fixed typo

--- app/views/emails/postCreateAuthor.blade.php
+++ app/views/emails/postCreateAuthor.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts/mail')
 @section('mailContent')
-    An Auhtor with your email-address was added on our plattform 
+    An Author with your email-address was added on our plattform 
     {{link_to_action('LoginController@showForm', 'PHDPapertool', array(), array())}}
     .
 @stop
